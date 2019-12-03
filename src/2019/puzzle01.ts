fba4175d3126c5c539f23238e2b6bbb4d3e70c9b
Dudrie <Dudrie@gmx.de>
2019-12-03 06:56:29 +0100

    Add solution for day 1.

---
+++ src/2019/puzzle01.ts
@@ -0,0 +1,21 @@
+import { readPuzzleInput, getLinesOfInput } from '../util/utils';
+
+function calcFuelOfModule(mod: number): number {
+  return Math.floor(mod / 3) - 2;
+}
+
+function calcFuelOfModuleAndFuel(mod: number): number {
+  const fuelOfModule = calcFuelOfModule(mod);
+
+  if (fuelOfModule < 0) {
+    return 0;
+  }
+
+  return fuelOfModule + calcFuelOfModuleAndFuel(fuelOfModule);
+}
+
+const input = readPuzzleInput(1);
+const modules: number[] = getLinesOfInput(input).map(line => Number.parseInt(line, 10));
+const totalFuel = modules.reduce((mass, module) => mass + calcFuelOfModuleAndFuel(module), 0);
+
+console.log(`A: Total fuel is ${totalFuel}`);
