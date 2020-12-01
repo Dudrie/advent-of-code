1ba9cddeaad352c8201c40f8e670e4c61e86083a
Dudrie <Dudrie@gmx.de>
2020-12-01 10:13:47 +0100

    Add PuzzleInputReader class.

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -3,6 +3,63 @@ import * as path from 'path';
 import pkg from '../../package.json';
 
 /**
+ * Used to load the puzzle inputs from files on the file system.
+ */
+export class PuzzleInputReader {
+  private readonly input: string;
+
+  /**
+   * Initiliazes the reader by loading the input of the given puzzle.
+   *
+   * Reads the corresponding input file of the given puzzle by translating the puzzleNumber to a string. If the puzzleNumber does not have 2 digits a leading 0 will be added.
+   *
+   * @param puzzleNumber Number of the puzzle of which the input file should be read
+   * @param testNr (default: 0) Determines if and which test input to load which is set by the number. The number has to be higher than 0 and there has to exist a file ending with 'TestXX' where 'XX' is the number.
+   */
+  constructor(private readonly puzzleNumber: number, private readonly testNr: number = 0) {
+    this.input = this.loadInput();
+  }
+
+  /**
+   * @returns The loaded puzzle input as one string.
+   */
+  public getPuzzleInput(): string {
+    return this.input;
+  }
+
+  /**
+   * @returns The loaded puzzle input split by lines.
+   */
+  public getPuzzleInputSplitByLines(): string[] {
+    return this.input.split(/\r\n|\r|\n/).filter((line) => line !== '');
+  }
+
+  private loadInput(): string {
+    const numString = this.getNumberAsString();
+    const testString: string =
+      this.testNr > 0 ? `Test${this.testNr.toString(10).padStart(2, '0')}` : '';
+
+    const fileContentBuffer: Buffer = fs.readFileSync(
+      path.join(
+        __dirname,
+        '..',
+        `${pkg.currentYear}`,
+        'input',
+        `puzzle${numString}${testString}.txt`
+      )
+    );
+
+    return fileContentBuffer.toString();
+  }
+
+  private getNumberAsString(): string {
+    return this.puzzleNumber.toString(10).padStart(2, '0');
+  }
+}
+
+/**
+ * @deprecated Is kept so old puzzle solutions don't break. Use the `PuzzleInputReader` class instead.
+ *
  * Reads the input of the given puzzle.
  *
  * Reads the corresponding input file of the given puzzle by translating the puzzleNumber to a string. If the puzzleNumber does not have 2 digits a leading 0 will be added. The input is returned as a single String.
@@ -13,37 +70,12 @@ import pkg from '../../package.json';
  * @returns Puzzle input as String
  */
 export function readPuzzleInput(puzzleNumber: number, testNr: number = 0): string {
-  const numString: string = getNumString(puzzleNumber, 2);
-
-  const testString: string = testNr > 0 ? `Test${getNumString(testNr, 2)}` : '';
-  const fileContentBuffer: Buffer = fs.readFileSync(
-    path.join(__dirname, '..', `${pkg.currentYear}`, 'input', `puzzle${numString}${testString}.txt`)
-  );
-
-  return fileContentBuffer.toString();
+  return new PuzzleInputReader(puzzleNumber, testNr).getPuzzleInput();
 }
 
 /**
- * Converts a number to a string with a given length.
+ * @deprecated Is kept so old puzzle solutions don't break. Use the `PuzzleInputReader` class instead.
  *
- * Converts the given number to a string with the specified length. If the number itself is to short the string gets enlarged by adding leading zeros.
- *
- * @param num Number to convert to a string
- * @param length Length the returned string should have
- *
- * @returns String of the given number with the given length.
- */
-function getNumString(num: number, length: number): string {
-  let numString: string = num + '';
-
-  while (numString.length < length) {
-    numString = '0' + numString;
-  }
-
-  return numString;
-}
-
-/**
  * Splits the given input by line.
  *
  * Each line of the given input will be a seperate entry in the returned array. It ignores empty lines.
@@ -52,7 +84,7 @@ function getNumString(num: number, length: number): string {
  * @returns Array with strings in which every (previous) line is an entry
  */
 export function getLinesOfInput(input: string): string[] {
-  return input.split(/\r\n|\r|\n/).filter(line => line !== '');
+  return input.split(/\r\n|\r|\n/).filter((line) => line !== '');
 }
 
 /**
