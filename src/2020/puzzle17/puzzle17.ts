a3e5894163dabd3039a854a495d1ca9bf56d5935
Dudrie <Dudrie@gmx.de>
2020-12-18 00:31:54 +0100

    Clean up code for 2020/P17.

--- src/2020/puzzle17/puzzle17.ts
+++ src/2020/puzzle17/puzzle17.ts
@@ -7,33 +7,20 @@ class PuzzleSolver17 extends PuzzleSolver {
   }
 
   solve(): void {
-    this.solveA();
-    this.solveB();
+    this.solveForDimension(3, 'A');
+    this.solveForDimension(4, 'B');
   }
 
-  private solveA(): void {
-    const space: Space = new Space(this.inputReader.getPuzzleInputSplitByLines(), 3);
+  private solveForDimension(dimension: number, part: 'A' | 'B'): void {
+    const space: Space = new Space(this.inputReader.getPuzzleInputSplitByLines(), dimension);
 
-    console.log('Running cycle for part A');
-    this.runCycleForSpace(space);
-
-    this.printSolution(space.getActiveCubeCount(), 'A');
-  }
-
-  private solveB(): void {
-    const space: Space = new Space(this.inputReader.getPuzzleInputSplitByLines(), 4);
-
-    console.log('Running cycle for part B');
-    this.runCycleForSpace(space);
-
-    this.printSolution(space.getActiveCubeCount(), 'B');
-  }
-
-  private runCycleForSpace(space: Space): void {
+    console.log(`Running cycle for part ${part}`);
     for (let i = 0; i < 6; i++) {
       space.runTick();
       console.log(`Active cubes after cycle ${i + 1}: ${space.getActiveCubeCount()}`);
     }
+
+    this.printSolution(space.getActiveCubeCount(), 'A');
   }
 }
 
